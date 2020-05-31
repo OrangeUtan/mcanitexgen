acee0763aa89863168b131494bdc1412cd5b39b4
OraneUtan <Oran9eUtan@gmail.com>
2020-05-31 22:15:01 +0200

    Added long description and fixed License classifier

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r") as file:
+	long_description = file.read()
+
 setup(
 	name='mcmetagen',
-	version='0.0.1',
+	version='0.0.2',
 	description='A texture animation generator for Minecraft .mcmeta files',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
 	py_modules=["mcmetagen"],
 	package_dir={'': 'src'},
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"Programming Language :: Python :: 3.6",
 		"Programming Language :: Python :: 3.7",
-		"License :: MIT License"
+		"License :: OSI Approved :: MIT License"
 	],
 	extras_require = {
 		"dev": [
