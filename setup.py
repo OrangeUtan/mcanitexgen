297885bed36f7520bd66e19f4be8293188d08024
Oran9eUtan <oran9eutan@gmail.com>
2020-06-09 22:19:19 +0200

    v0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
 	name='mcanitexgen',
-	version='0.0.4',
+	version='0.0.6',
 	author='OrangeUtan',
 	author_email='oran9eutan@gmail.com',
 	description='A texture animation generator for Minecraft .mcmeta files',
