46a293420d2457142b172f2b2291513a3062ec0e
Oran9eUtan <oran9eutan@gmail.com>
2020-06-09 16:17:40 +0200

    Added methods to generate animation files

--- src/mcmetagen/Parser.py
+++ src/mcmetagen/Parser.py
@@ -4,17 +4,29 @@ from mcmetagen.TextureAnimation import *
 
 class Parser:
 	@classmethod
-	def _parse_animation_file(cls, path:str) -> Dict[str,TextureAnimation]:
-		json = Parser._load_yaml_file(path)
-		texture_animations = dict()
-		for name, ta_json in json.items():
-			texture_animations[name] = TextureAnimation.from_json(name, ta_json, texture_animations)
-
-		return texture_animations
+	def generate_animations(cls, dir:str = "."):
+		for path in Parser._get_animation_files_in_dir(dir):
+			Parser.generate_animations_from_file(path)
 
 	@classmethod
-	def _animated_entry_to_frames(cls, entry: AnimatedEntry) -> Generator[Dict, None, None]:
-		return Parser._combine_consecutive_frames(entry.to_frames())
+	def generate_animations_from_file(cls, path:str):
+		for _,texture_animation in Parser._parse_animation_file(path).items():
+			if texture_animation.texture:
+				anim = Parser._animation_from_texture_animation(texture_animation)
+				outfile = f"{os.path.join(os.path.dirname(path), texture_animation.texture)}.mcmeta"
+				with open(outfile, "w+") as file:
+					json.dump(anim, file, indent=4)
+
+	@classmethod
+	def _animation_from_texture_animation(cls, anim: TextureAnimation):
+		frames = list(Parser._combine_consecutive_frames(anim.animation.to_frames()))
+		return {
+			"animation": {
+				"interpolate": anim.interpolate,
+				"frametime": 1,
+				"frames": frames
+			}
+		}
 
 	@classmethod
 	def _combine_consecutive_frames(cls, frames:Iterable[Dict]) -> Dict:
@@ -38,6 +50,15 @@ class Parser:
 			animation_files = filter(lambda file: file.endswith(".animation.yml"), files)
 			for file in map(lambda file: os.path.join(root, file), animation_files):
 				yield file
+
+	@classmethod
+	def _parse_animation_file(cls, path:str) -> Dict[str,TextureAnimation]:
+		json = Parser._load_yaml_file(path)
+		texture_animations = dict()
+		for name, ta_json in json.items():
+			texture_animations[name] = TextureAnimation.from_json(name, ta_json, texture_animations)
+
+		return texture_animations
 
 	@classmethod
 	def _load_yaml_file(cls, path):
