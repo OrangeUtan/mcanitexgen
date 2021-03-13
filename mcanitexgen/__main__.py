55bb6e6d781403109f159aa99fd22e21ed89c075
Oran9eUtan <Oran9eUtan@gmail.com>
2021-03-13 16:21:27 +0100

    Added command to create gif from animations

--- mcanitexgen/__main__.py
+++ mcanitexgen/__main__.py
@@ -1,4 +1,5 @@
 import json
+import os
 from pathlib import Path
 from typing import Optional
 
@@ -10,18 +11,17 @@ app = typer.Typer()
 
 
 @app.command()
-def main(
+def generate(
     animations_file: str,
     out_dir: Optional[str] = typer.Argument(
         None, help="Directory animation files will be generated in"
     ),
 ):
     animations_file: Path = Path(animations_file)
-    out_dir: Path = Path(out_dir) if out_dir else animations_file.parent
-    out_dir.mkdir(parents=True, exist_ok=True)
-
     if not animations_file.exists():
         raise FileNotFoundError(animations_file)
+    out_dir: Path = Path(out_dir) if out_dir else animations_file.parent
+    out_dir.mkdir(parents=True, exist_ok=True)
 
     texture_animations = load_animations_from_file(animations_file)
     for animation in texture_animations.values():
@@ -31,6 +31,54 @@ def main(
             }
 
             json.dump(data, f)
+
+
+def get_animation_states(img):
+    state_size, img_height = img.size
+    assert state_size % 2 == 0
+    assert img_height % state_size == 0
+    num_states = int(img_height / state_size)
+
+    return [
+        img.crop((0, i * state_size, state_size, (i + 1) * state_size))
+        for i in range(num_states)
+    ]
+
+
+@app.command()
+def gif(
+    animations_file: str,
+    out_dir: Optional[str] = typer.Argument(
+        None, help="Directory gif files will be generated in"
+    ),
+):
+    import PIL.Image
+    from PIL.Image import Image
+
+    import mcanitexgen.images2gif
+
+    animations_file: Path = Path(animations_file)
+    if not animations_file.exists():
+        raise FileNotFoundError(animations_file)
+    out_dir: Path = Path(out_dir) if out_dir else animations_file.parent
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    for animation in load_animations_from_file(animations_file).values():
+        texture_path = Path(animations_file.parent, animation.texture)
+        gif_path = Path(out_dir, f"{os.path.splitext(animation.texture.name)[0]}.gif")
+
+        states = get_animation_states(PIL.Image.open(texture_path))
+        frametime = 1 / 20
+
+        frames: list[Image] = []
+        durations = []
+        for frame in animation.frames:
+            frames.append(states[frame["index"]])
+            durations.append(frametime * frame["time"])
+
+        mcanitexgen.images2gif.writeGif(
+            gif_path, images=frames, duration=durations, subRectangles=True
+        )
 
 
 app()
