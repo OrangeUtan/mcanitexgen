7aa600704cc5e0d6afe26ce2cd63120e96455565
Oran9eUtan <oran9eutan@gmail.com>
2021-03-13 02:14:19 +0100

    Fixed integration tests

--- tests/integration/test_single_animations.py
+++ tests/integration/test_single_animations.py
@@ -1,7 +1,7 @@
 import ruamel.yaml as yaml
 
-from mcanitexgen import generator, parser
-from mcanitexgen.generator import Animation
+from mcanitexgen import Sequence, State, animation
+from mcanitexgen.generator import Animation, TextureAnimation
 
 
 def frame(index: int, time: int):
@@ -9,26 +9,16 @@ def frame(index: int, time: int):
 
 
 def test_simple_blinking():
-    json = yaml.safe_load(
-        """
-    head.png:
-        states: [
-            NEUTRAL_OPEN,
-            NEUTRAL_CLOSED,
-        ]
+    @animation("head.png")
+    class Head(TextureAnimation):
+        NEUTRAL_OPEN = State(0)
+        NEUTRAL_CLOSED = State(1)
 
-        blink():
-            - NEUTRAL_OPEN: {duration: 120}
-            - NEUTRAL_CLOSED: {duration: 4}
+        blink = Sequence(NEUTRAL_OPEN(duration=120), NEUTRAL_CLOSED(duration=4))
 
-        blinking():
-            - 3 * blink()
+        blinking = Sequence(3 * blink)
 
-        main(): [
-            blinking()
-        ]
-    """
-    )
+        main = Sequence(blinking)
 
     expected_anim = Animation(
         0,
@@ -43,32 +33,23 @@ def test_simple_blinking():
         ],
     )
 
-    animation = generator.create_animation(parser.parse_animations(json)["head"])
-    assert animation == expected_anim
-    assert sum(map(lambda f: f["time"], animation.frames)) == expected_anim.end
+    assert Head.start == expected_anim.start
+    assert Head.end == expected_anim.end
+    assert Head.frames == expected_anim.frames
+    assert sum(map(lambda f: f["time"], Head.frames)) == expected_anim.end
 
 
 def test_weighted_blinking():
-    json = yaml.safe_load(
-        """
-    head.png:
-        states: [
-            NEUTRAL_OPEN,
-            NEUTRAL_CLOSED,
-        ]
+    @animation("head.png")
+    class Head(TextureAnimation):
+        NEUTRAL_OPEN = State(0)
+        NEUTRAL_CLOSED = State(1)
 
-        blink():
-            - NEUTRAL_OPEN: {weight: 5}
-            - NEUTRAL_CLOSED: {weight: 1}
+        blink = Sequence(NEUTRAL_OPEN(weight=5), NEUTRAL_CLOSED(weight=1))
 
-        blinking():
-            - 3 * blink(): {duration: 120}
+        blinking = Sequence(3 * blink(duration=120))
 
-        main(): [
-            blinking()
-        ]
-    """
-    )
+        main = Sequence(blinking)
 
     expected_anim = Animation(
         0,
@@ -83,77 +64,60 @@ def test_weighted_blinking():
         ],
     )
 
-    animation = generator.create_animation(parser.parse_animations(json)["head"])
-    assert animation == expected_anim
-    assert sum(map(lambda f: f["time"], animation.frames)) == expected_anim.end
+    assert Head.end == expected_anim.end
+    assert Head.start == expected_anim.start
+    assert Head.frames == expected_anim.frames
+    assert sum(map(lambda f: f["time"], Head.frames)) == expected_anim.end
 
 
 def test_weighted_blinking_2():
-    json = yaml.safe_load(
-        """
-    head.png:
-        states: [
-            NEUTRAL_OPEN,
-            NEUTRAL_CLOSED,
-        ]
+    @animation("head.png")
+    class Head(TextureAnimation):
+        NEUTRAL_OPEN = State(0)
+        NEUTRAL_CLOSED = State(1)
 
-        blinking():
-            - 2 * blink(): {weight: 1}
-            - NEUTRAL_CLOSED: {duration: 10}
+        blink = Sequence(NEUTRAL_OPEN(weight=1), NEUTRAL_CLOSED(duration=4))
 
-        blink():
-            - NEUTRAL_OPEN: {weight: 1}
-            - NEUTRAL_CLOSED: {duration: 4}
+        blinking = Sequence(2 * blink(weight=1), NEUTRAL_CLOSED(duration=4))
 
-        main(): [
-            2 * blinking(): {duration: 39}
-        ]
-    """
-    )
+        main = Sequence(2 * blinking(duration=39))
 
     expected_anim = Animation(
         0,
         78,
         [
-            frame(0, 11),
+            frame(0, 14),
             frame(1, 4),
-            frame(0, 10),
+            frame(0, 13),
             frame(1, 4),
-            frame(1, 10),
+            frame(1, 4),
             #
-            frame(0, 11),
+            frame(0, 14),
             frame(1, 4),
-            frame(0, 10),
+            frame(0, 13),
             frame(1, 4),
-            frame(1, 10),
+            frame(1, 4),
         ],
     )
 
-    animation = generator.create_animation(parser.parse_animations(json)["head"])
-    assert animation == expected_anim
-    assert sum(map(lambda f: f["time"], animation.frames)) == expected_anim.end
+    assert Head.start == expected_anim.start
+    assert Head.end == expected_anim.end
+    assert len(Head.frames) == len(expected_anim.frames)
+    assert Head.frames == expected_anim.frames
+    assert sum(map(lambda f: f["time"], Head.frames)) == expected_anim.end
 
 
 def test_unweighted_seq_in_weighted_seq():
-    json = yaml.safe_load(
-        """
-    head.png:
-        states: [
-            A,
-            B,
-        ]
+    @animation("head.png")
+    class Head(TextureAnimation):
+        A = State(0)
+        B = State(1)
 
-        nested_unweighted():
-            - A: {duration: 4}
+        nested_unweighted = Sequence(A(duration=4))
 
-        weighted():
-            - B: {weight: 1}
-            - nested_unweighted()
+        weighted = Sequence(B(weight=1), nested_unweighted)
 
-        main():
-            - weighted(): {duration: 120}
-    """
-    )
+        main = Sequence(weighted(duration=120))
 
     expected_anim = Animation(
         0,
@@ -164,5 +128,8 @@ def test_unweighted_seq_in_weighted_seq():
         ],
     )
 
-    animation = generator.create_animation(parser.parse_animations(json)["head"])
-    assert animation == expected_anim
+    assert Head.start == expected_anim.start
+    assert Head.end == expected_anim.end
+    assert len(Head.frames) == len(expected_anim.frames)
+    assert Head.frames == expected_anim.frames
+    assert sum(map(lambda f: f["time"], Head.frames)) == expected_anim.end
