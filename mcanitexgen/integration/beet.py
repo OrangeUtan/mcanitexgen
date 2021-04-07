366dad8500221d6b5c9cdc92bf30607352dd1dc1
Oran9eUtan <Oran9eUtan@gmail.com>
2021-04-07 17:04:25 +0200

    fix: Generate .mcmeta files in correct location

--- mcanitexgen/integration/beet.py
+++ mcanitexgen/integration/beet.py
@@ -1,9 +1,12 @@
+import logging
 from pathlib import Path
 from typing import Iterable
 
 from beet import Context, TextureMcmeta
 
 import mcanitexgen
+
+logger = logging.getLogger(__name__)
 
 
 def beet_default(ctx: Context):
@@ -17,28 +20,20 @@ def beet_default(ctx: Context):
 
 def create_mcanitexgen_plugin(load: Iterable[str] = ()):
     def plugin(ctx: Context):
-        minecraft = ctx.assets["minecraft"]
-
         for pattern in load:
             for path in ctx.directory.glob(pattern):
                 animations = mcanitexgen.animation.load_animations_from_file(path)
 
-                minecraft.textures_mcmeta.merge(
-                    {
-                        get_mcmeta_path(ctx.directory, path, texanim.texture): TextureMcmeta(
-                            texanim.to_mcmeta()
+                for anim in animations.values():
+                    texture = Path(anim.texture.parent, anim.texture.stem).as_posix()
+                    if not texture in ctx.assets.textures:
+                        logger.error(
+                            f"Texture '{anim.texture}' referenced by {path.stem + path.suffix}::{anim.__name__} does not exist"
                         )
-                        for _, texanim in animations.items()
-                    }
-                )
+                        continue
+
+                    ctx.assets.textures_mcmeta.merge(
+                        {texture: TextureMcmeta(anim.to_mcmeta())}
+                    )
 
     return plugin
-
-
-def get_mcmeta_path(ctx_path: Path, animation_file_path: Path, texture_path: Path):
-    # Remove suffix
-    texture_path = Path(texture_path.parent, texture_path.stem)
-    # Get texture relative to animation file
-    texture_path = Path(animation_file_path.parent, texture_path).resolve()
-
-    return texture_path.relative_to(ctx_path).as_posix()
--- tests/integration/beet/examples/mc_folder_structure/resourcepack/assets/minecraft/textures/item/example1.animation.py
+++ tests/integration/beet/examples/mc_folder_structure/resourcepack/assets/minecraft/textures/item/example1.animation.py
@@ -1,7 +1,7 @@
 from mcanitexgen.animation import Sequence, State, TextureAnimation, animation
 
 
-@animation("../example1.png")
+@animation("minecraft:item/example1.png")
 class Example1(TextureAnimation):
     A = State(0)
     B = State(1)
